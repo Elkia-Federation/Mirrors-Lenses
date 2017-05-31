8f340e0a2c2ad204d651545ae4d541e729a56818
ITotallyExist <gregoirebrougher@gmail.com>
2017-05-31 15:28:43 -0700

    ode

--- main.js
+++ main.js
@@ -1,10 +1,37 @@
+var startRay = [0,0,0];
+var lenses = [];
+
 function setup (){
     resize();
     setTimeout(function(){refresh();}, 10);
 }
 
-function refresh (x){
-    setTimeout(function(x){refresh();}, 10);
+function refresh (){
+    var ray = startRay;
+    var lowestDistance = [];
+    while (lowestDistance != [-1]){
+        lowestDistance = [-1];
+        for (i=0; i<lenses.length; i++){
+            var intersect = lenses[i].testIntersect(ray);
+            if (intersect[0]){
+                if (intersect[1] > lowestDistance[0]){
+                    lowestDistance = [intersect[1], i];
+                }
+            }
+        }
+        if (lowestDistance != [-1]){
+            ray = doLens(ray);
+        }
+    }
+    
+    var canvas = document.getElementById("canvas");
+    var ctx = canvas.getContext;
+    
+    for (i = 0; i < lenses.length; i++){
+        lenses[i].draw(ctx);
+    }
+    
+    setTimeout(function(){refresh();}, 10);
     
 }
 
